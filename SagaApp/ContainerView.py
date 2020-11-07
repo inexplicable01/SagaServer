5bd698865670de0e8c38fb2b24560810f7c43eeb
Waichak Luk <waichak.luk@gmail.com>
2020-11-07 02:24:50 -0500

    Resetting the repository in Sagaserver

--- SagaApp/ContainerView.py
+++ SagaApp/ContainerView.py
@@ -12,15 +12,28 @@ import re
 
 class ContainerView(Resource):
 
+    def latestRev(self, path):
+        rev = 0;
+        for fn in os.listdir(path):
+            m = re.search('Rev(\d+).yaml',fn)
+            if  int(m.group(1))>rev:
+                revnum = int(m.group(1))
+                latestrev = fn
+        return latestrev, revnum
+
     def __init__(self, rootpath):
         self.rootpath = rootpath
 
     def get(self):
         containerID = request.form['containerID']
+        branch ='Main'
 
         if os.path.exists(safe_join(self.rootpath, 'Container', containerID)):
+            latestrevfn, revnum = self.latestRev(safe_join(self.rootpath, 'Container', containerID, branch))
             result = send_from_directory(safe_join(self.rootpath, 'Container', containerID), 'containerstate.yaml' )
             result.headers['file_name'] = 'containerstate.yaml'
+            result.headers['branch'] = branch
+            result.headers['revnum'] = str(revnum)
             return result
         else:
             return {"response": "Invalid Container ID"}
--- SagaApp/FrameView.py
+++ SagaApp/FrameView.py
@@ -73,9 +73,8 @@ class FrameView(Resource):
                 filetrackobj.style = updateinfo[ContainerObjName]['style']
                 filetrackobj.file_id = uuid.uuid4().__str__()
                 filetrackobj.commitUTCdatetime = committime
-                fileb = request.files[ContainerObjName].read()
-                with open(os.path.join(self.rootpath, 'Files', filetrackobj.file_id), 'wb') as newfile:
-                    newfile.write(fileb)
+                request.files[ContainerObjName].save(os.path.join(self.rootpath, 'Files', filetrackobj.file_id))
+
                 # print(filetrackobj.file_name)
                 # print(request.files[filetrackobj.file_name])
 
